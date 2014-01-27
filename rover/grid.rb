17efd2b2bbc0d07dadd51327f2c5e7cce808410b
Sep Bar <sepand.barkhodaee@gmail.com>
2014-01-26 19:23:50 -0500

    rover

---
+++ rover/grid.rb
@@ -0,0 +1,38 @@
+require_relative 'rover'
+
+class Grid
+
+	attr_accessor :gridx, :gridy
+
+	def initialize(gridx, gridy)
+		@gridx = gridx
+		@gridy = gridy
+	end
+
+	def add_rover(x, y, direction, instructions)
+		r = Rover.new(x, y, direction, instructions)
+		r.final_position
+
+		puts "ROVER POSITION:"
+
+		if r.x>@gridx ||r.y>@gridy
+			puts "This rover has ventured off too far!"
+			puts ""
+		else
+			puts "x: #{r.x}"
+			puts "y: #{r.y}"
+			puts "direction: #{r.direction}"
+			puts ""
+		end
+
+	end
+
+
+end
+
+grid = Grid.new(5, 5)
+
+#change coordinates/directions/instructions below
+grid.add_rover(1,2, "N", "LMLMLMLMM")
+grid.add_rover(3,3, "E", "MMRMMRMRRM")
+
---
+++ rover/rover.rb
@@ -0,0 +1,77 @@
+class Rover
+	attr_accessor :x, :y, :direction, :instructions
+
+	def initialize(x, y, direction, instructions)
+		@x = x
+		@y = y
+		@direction = direction
+		@instructions = instructions.split("")
+	end
+
+	def final_position
+		@instructions.length.times do
+			case @direction
+			when "N"
+				case @instructions[0]
+				when "M"
+					@y += 1
+					@instructions.delete_at(0)
+				when "L"
+					@direction = "W"
+					@instructions.delete_at(0)
+
+				when "R"
+					@direction = "E"
+					@instructions.delete_at(0)
+				end
+			when "E"
+				case @instructions[0]
+				when "M"
+					@x += 1
+					@instructions.delete_at(0)
+				when "L"
+					@direction = "N"
+					@instructions.delete_at(0)
+
+				when "R"
+					@direction = "S"
+					@instructions.delete_at(0)
+				end
+			when "S"
+				case @instructions[0]
+				when "M"
+					@y -= 1
+					@instructions.delete_at(0)
+				when "L"
+					@direction = "E"
+					@instructions.delete_at(0)
+
+				when "R"
+					@direction = "W"
+					@instructions.delete_at(0)
+				end
+			when "W"
+				case @instructions[0]
+				when "M"
+					@x -= 1
+					@instructions.delete_at(0)
+				when "L"
+					@direction = "S"
+					@instructions.delete_at(0)
+
+				when "R"
+					@direction = "N"
+					@instructions.delete_at(0)
+				end
+			end
+		end
+		# end
+
+
+	end
+
+
+end
+
+# rover1 = Rover.new(1,2, "N", "LMLMLMLMM")
+# rover2 = Rover.new(3,3, "E", "MMRMMRMRRM")
